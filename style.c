926bfcf9ca2dae01d2fd61190f91f32360008b65
rohanmetangale-byte <rohan.metangale@gmail.com>
2025-12-16 10:01:33 +0530

    Web.css
    added web.css

---
+++ style.c
@@ -0,0 +1,19 @@
+p,h3{
+ font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color:antiquewhite;
+  font-size: larger;
+  color:darkred;
+  text-decoration: double;
+  
+}                                                                             
+#section
+{                                                                              
+  font: size 28px;
+}
+body{
+  background-image: url(https://wallpapercave.com/wp/wp9800007.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       {background-image: radial-gradient(beige,aqua,yellow);}
+
